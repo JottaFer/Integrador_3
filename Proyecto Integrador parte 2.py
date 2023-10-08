aacca737d271f4bc07366dea3ee3b0d04ee11adf
pedro fernandez <peferna02@gmail.com>
2023-10-07 20:00:01 -0500

    Terminado

--- Proyecto Integrador parte 2.py
+++ Proyecto Integrador parte 2.py
@@ -1,7 +1,9 @@
-from readchar import readkey,key # de esta manera importas solo esas dos que es lo que necesitas
+#Se inicia colocando las dos acciones que vamos a utilizar
+
+from readchar import readkey,key 
 input_str = ""
 while True:
-    leido = readkey() # Se ingresara la tecla a presionar
+    leido = readkey() # Se 
     print("leer tecla", leido)
     (input_str) += leido
     if leido == key.UP: # acá le das la instrucción de que al precionar la tecla arriba que es lo que va realizar

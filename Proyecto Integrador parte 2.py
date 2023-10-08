bb21d80b237ffb5f01598c0b2565b84ed10da96e
pedro fernandez <peferna02@gmail.com>
2023-10-07 19:48:58 -0500

    Cambio4

--- Proyecto Integrador parte 2.py
+++ Proyecto Integrador parte 2.py
@@ -2,8 +2,7 @@ from readchar import readkey,key
 input_str = ""
 while True:
     leido = readkey()
+    print("leer la tecla", leido)
     (input_str) += leido
     if leido == key.UP:
-        break
-
-
+     break

4635813c1aa0172d2086d0a15f585bda5c1e3737
pedro fernandez <peferna02@gmail.com>
2023-10-07 19:52:51 -0500

    cambio5

--- Proyecto Integrador parte 2.py
+++ Proyecto Integrador parte 2.py
@@ -1,12 +1,3 @@
-"""from readchar import readkey,key
-input_str = ""
-while True:
-    leido = readkey()
-    print("leer la tecla", leido)
-    (input_str) += leido
-    if leido == key.UP:
-     break"""
-
 from readchar import readkey,key # de esta manera importas solo esas dos que es lo que necesitas
 input_str = ""
 while True:
@@ -14,4 +5,4 @@ while True:
     print("leer tecla", leido)
     (input_str) += leido
     if leido == key.UP: # acá le das la instrucción de que al precionar la tecla arriba que es lo que va realizar
-        break
+     break

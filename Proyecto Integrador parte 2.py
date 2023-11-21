55aef8a5401a147a68198ee680c5c83fd0d5f4ea
pedro fernandez <peferna02@gmail.com>
2023-11-21 17:13:18 -0500

    Entrega de proyecto

--- Proyecto Integrador parte 2.py
+++ Proyecto Integrador parte 2.py
@@ -1,3 +1,14 @@
+"""#Se inicia colocando las dos acciones que vamos a utilizar
+
+from readchar import readkey,key 
+input_str = ""
+while True:
+    leido = readkey() # Se nombra la variable con la accion (en este caso, "presionar una tecla en especifico del teclado")
+    print("leer tecla", leido)
+    (input_str) += leido
+    if leido == key.UP: # Despues de haber dado la tarea a realizar, se le indica qué tecla se debe presionar para finalizar
+     break"""
+
 #Se inicia colocando las dos acciones que vamos a utilizar
 
 from readchar import readkey,key 
